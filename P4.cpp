11085c08bdff1fb178fd5aac6dd80a8558d9956c
Daniel <dangamecorp@gmail.com>
2026-03-16 13:18:40 +1100

    fixed warnings- variables not used in arguments

--- P4.cpp
+++ P4.cpp
@@ -12,6 +12,10 @@ int main()
     double speed = 0.0;
     double time = 0.0;
     double distance = 0.0;
+    double remaining_distance = 0.0;
+    double total_distance, total_time = 0.0;
+    double time_to_destination = 0.0;
+    
 
     string user_input;
 
@@ -27,7 +31,23 @@ int main()
     user_input = read_line();
     time = to_double(user_input);
 
+    speed = distance / time / MINUTES_PER_HOUR;
     write("Your speed is:");
-    write(distance / time);
+    write(speed);
     write("km/hr");
+
+    write_line();
+    write("How far do you have to go? Enter km: ");
+    user_input = read_line();
+    remaining_distance = to_double(user_input);
+
+    time_to_destination = remaining_distance / speed * MINUTES_PER_HOUR;
+
+    write_line("You will take another " + to_string(time_to_destination) + " minutes before you arrive");
+
+    total_distance = distance + remaining_distance;
+    total_time = time + time_to_destination;
+
+    write_line("Total distance will be " + to_string(total_distance) + " km");
+    write_line("Total time will be " + to_string(total_time) + "minutes");
 }
